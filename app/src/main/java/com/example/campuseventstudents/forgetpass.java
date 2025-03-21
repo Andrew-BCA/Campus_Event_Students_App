9b7b9cf97796b37ee8ad253d2fb9a1e984992c65
andrewpatrick011@gmail.com <andrewpatrick011@gmail.com>
2025-03-21 06:49:37 +0530

    commit

--- app/src/main/java/com/example/campuseventstudents/forgetpass.java
+++ app/src/main/java/com/example/campuseventstudents/forgetpass.java
@@ -125,39 +125,7 @@ public class forgetpass extends AppCompatActivity {
 
 
 
-   /* private void sendEmail() {
-        // Replace with your Gmail
-        final String senderEmail = "andrewpatrick011@gmail.com";
-        final String senderPassword = "gfeo gryv irqr stoq";
 
-        String email = emailEditText.getText().toString().trim();
-
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(senderEmail, senderPassword);
-            }
-        });
-
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(senderEmail));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-            message.setSubject("Password Reset OTP");
-            message.setText("Your OTP for password reset is: " + otp);
-
-            Transport.send(message);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-            Toast.makeText(forgetpass.this, "Failed to send OTP", Toast.LENGTH_SHORT).show();
-        }
-    }*/
 
     private void sendEmail() {
         final String email = emailEditText.getText().toString().trim();
